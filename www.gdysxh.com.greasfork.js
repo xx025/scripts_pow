81d3d24a58036e767eed940b5eb35f6133305ee4
xx025 <71559822+xx025@users.noreply.github.com>
2022-01-26 02:01:23 +0800

    s

---
+++ www.gdysxh.com.greasfork.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         gdysxh_auto_videos
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  医药协会_继续教育_我的课程
+// @author       You
+// @match        https://www.gdysxh.com/my_classes/classes/index.html
+// @match        https://www.gdysxh.com/my_classes/classes/video/course_id/*
+// @icon         https://www.google.com/s2/favicons?domain=gdysxh.com
+// @grant        none
+// ==/UserScript==
+
---
+++ zzys.gdfda.org.greasfork.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         zyys_gdfda_org_auto_play_vidoes
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  执业药师管理系统 2021 [公需课] [科技创新现状与发展趋势]
+// @author       You
+// @match        https://zyys.gdfda.org/Home/User/enroll_les
+// @match        https://zyys.gdfda.org/Home/User/study/id/*
+// @icon         https://www.google.com/s2/favicons?domain=gdfda.org
+// @license      MIT
+// @grant        none
+// ==/UserScript==
+
+window.onload = (function () {
+    function appendJQCDN(src) {
+        var head = document.body || document.getElementsByTagName('body')[0];
+        if (src.indexOf("js") == -1) { //css
+            var style = document.createElement('style');
+            style.setAttribute("rel", "stylesheet");
+            style.setAttribute("href", src);
+            head.appendChild(style);
+        } else {
+            var script = document.createElement('script');
+            script.type = "text/javascript";
+            script.setAttribute("src", src);
+            head.appendChild(script);
+        }
+    };
+
+    let nocache = new Date().getTime();
+    appendJQCDN("https://greasyfork.org/scripts/439119-zyys-gdfda-org-auto-play-videos/code/zyys_gdfda_org_auto_play_videos.js?"+nocache)
+
+})();
