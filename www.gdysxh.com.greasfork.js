90ff370487b759e041001a5036816f912bf2ef1c
xx025 <71559822+xx025@users.noreply.github.com>
2022-01-26 02:12:56 +0800

    s

--- www.gdysxh.com.greasfork.js
+++ www.gdysxh.com.greasfork.js
@@ -10,3 +10,23 @@
 // @grant        none
 // ==/UserScript==
 
+window.onload = (function () {
+    function appendJQCDN(src) {
+        var head = document.body || document.getElementsByTagName('body')[0];
+        if (src.indexOf("js") == -1) { //css
+            var style = document.createElement('style');
+            style.setAttribute("rel", "stylesheet");
+            style.setAttribute("href", src);
+            head.appendChild(style);
+        } else {
+            var script = document.createElement('script');
+            script.type = "text/javascript";
+            script.setAttribute("src", src);
+            head.appendChild(script);
+        }
+    };
+
+    let nocache = new Date().getTime();
+    appendJQCDN("https://greasyfork.org/scripts/439126-gdysxh-auto-videos-source/code/gdysxh_auto_videos_source.js?"+nocache)
+
+})();
