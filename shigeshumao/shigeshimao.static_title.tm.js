af9b666520cc373673c4d1df3fed6f2d30eb53f9
xx025 <71559822+xx025@users.noreply.github.com>
2022-02-26 00:41:54 +0800

    Update shigeshimao.static_title.tm.js

--- shigeshumao/shigeshimao.static_title.tm.js
+++ shigeshumao/shigeshimao.static_title.tm.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name         shigeshimao.static_title
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2.2022年2月26日
 // @description  try to take over the world!
-// @author       You
+// @author       osci
 // @match        http://220.197.177.18:61235/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=177.18
-// @require      file:///E:\github\scripts_pow\shigeshumao\shigeshimao_nu.js
+// @require      file:///E:\github\scripts_pow\shigeshumao\shigeshimao.static_title.greasfork.js
 // @grant        none
 // ==/UserScript==
-
 
 
 
