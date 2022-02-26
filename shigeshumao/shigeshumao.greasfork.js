0990583dfcb17a600228fa4bb8c0914625948304
xx025 <71559822+xx025@users.noreply.github.com>
2022-02-26 14:07:44 +0800

    Update shigeshumao.greasfork.js

--- shigeshumao/shigeshumao.greasfork.js
+++ shigeshumao/shigeshumao.greasfork.js
@@ -15,20 +15,6 @@ let price = 9;
 
 
 $(document).ready(function () {
-    let my_css = '<style>.topBtn{\n' + 'position: fixed;\n' + 'bottom: 50px;\n' + 'right: 50px;\n' + 'width: 60px;\n' + 'height: 60px;\n' + 'line-height: 60px;\n' + 'text-align: center;\n' + 'color: #FFF;\n' + 'z-index: 999;\n' + 'background: #FF5722;\n' + 'cursor: pointer;\n' + 'border-radius: 30px;\n' + 'box-shadow:0px 0px 20px #000;' + '}' + '    #my_box_script {\n' + '        position: fixed;\n' + '        top: 200px;\n' + '        left: 400px;\n' + '        background-color: aliceblue;\n' + '        height: 400px;\n' + '        width: 800px;\n' + '        padding: 20px;\n' + '        display:none;' + '    }\n' + '    #my_box_script textarea {\n' + '        width: 755px;\n' + '        height: 320px;\n' + '        resize: block;\n' + '    }</style>'
-    $('head').append(my_css);
-    $('body').append('<div id="topBtn1" class="topBtn" style="bottom: 150px" >展开</div>');
-    $("body").append('<div id="my_box_script">' + '<textarea id="input_date"></textarea>' + '<button id="dao_ru_my">导入</button>' + '<p id="dig_o"></p>' + '</div>');
-
-    $("#topBtn1").click(function () {
-        let dis = $("#my_box_script").css("display")
-        if (dis === 'none') {
-            $("#my_box_script").css("display", 'block')
-        } else {
-            $("#my_box_script").css("display", 'none')
-        }
-    })
-
 
     function local_storage_set(storage_name, data) {
         localStorage.setItem(storage_name, JSON.stringify(data));
@@ -44,18 +30,6 @@ $(document).ready(function () {
             return JSON.parse(my_flag_list);
         }
     }
-
-    $("#dao_ru_my").click(function () {
-        let data = $("#my_box_script textarea").val();
-        let data_list = data.split('\n')
-        if (data_list.length > 0) {
-            $("#dig_o").text("成功导入" + data_list.length + "条；三秒后网页重载")
-            local_storage_set("my_title_list", data_list)
-            setTimeout(function () {
-                location.reload()
-            }, 3000)
-        }
-    })
 
 
     function get_flag(e) {
@@ -79,10 +53,43 @@ $(document).ready(function () {
                 let flag = my_flag_list.length
                 my_flag_list[flag] = flag
                 local_storage_set(storage_name, my_flag_list)
-                return flag
+
+                if (flag >= e.length) {
+                    alert("游标越界；所有的title已经被使用")
+                } else {
+                    return flag
+                }
+
             }
         }
     }
+
+    let my_css = '<style>.topBtn{\n' + 'position: fixed;\n' + 'bottom: 50px;\n' + 'right: 50px;\n' + 'width: 60px;\n' + 'height: 60px;\n' + 'line-height: 60px;\n' + 'text-align: center;\n' + 'color: #FFF;\n' + 'z-index: 999;\n' + 'background: #FF5722;\n' + 'cursor: pointer;\n' + 'border-radius: 30px;\n' + 'box-shadow:0px 0px 20px #000;' + '}' + '    #my_box_script {\n' + '        position: fixed;\n' + '        top: 200px;\n' + '        left: 400px;\n' + '        background-color: aliceblue;\n' + '        height: 400px;\n' + '        width: 800px;\n' + '        padding: 20px;\n' + '        display:none;' + '    }\n' + '    #my_box_script textarea {\n' + '        width: 755px;\n' + '        height: 320px;\n' + '        resize: block;\n' + '    }</style>'
+    $('head').append(my_css);
+    $('body').append('<div id="topBtn1" class="topBtn" style="bottom: 150px" >展开</div>');
+    $("body").append('<div id="my_box_script">' + '<textarea id="input_date"></textarea>' + '<button id="dao_ru_my">导入</button>' + '<p id="dig_o"></p>' + '</div>');
+
+    $("#topBtn1").click(function () {
+        let dis = $("#my_box_script").css("display")
+        if (dis === 'none') {
+            $("#my_box_script").css("display", 'block')
+        } else {
+            $("#my_box_script").css("display", 'none')
+        }
+    })
+
+    $("#dao_ru_my").click(function () {
+        let data = $("#my_box_script textarea").val();
+        let data_list = data.split('\n')
+        if (data_list.length > 0) {
+            $("#dig_o").text("成功导入" + data_list.length + "条；三秒后网页重载")
+            local_storage_set("my_title_list", data_list)
+            setTimeout(function () {
+                location.reload()
+            }, 3000)
+        }
+    })
+
 
     let urls = location.href
 
