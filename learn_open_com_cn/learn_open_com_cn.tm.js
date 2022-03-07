8be5acfd057946510cc1a51334a92faf11f42be5
xx025 <71559822+xx025@users.noreply.github.com>
2022-03-07 18:15:51 +0800

    Create learn_open_com_cn.tm.js

---
+++ learn_open_com_cn/learn_open_com_cn.tm.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         aopeng
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  奥鹏
+// @author       You
+// @match        https://learn.open.com.cn/StudentCenter/CourseWare/ShowResource*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=open.com.cn
+// @require      file:///E:\github\scripts_pow\learn_open_com_cn\learn_open_com_cn.gf.js
+// @grant        none
+// ==/UserScript==
